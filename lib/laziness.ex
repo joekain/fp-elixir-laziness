675f7bf806a59ca82830b510cc850728444a5c91
Joseph Kain <joekain@gmail.com>
2014-07-19 17:26:15 -0700

    Ex7 - map
    This required me to fix a problem with the implementation.  The tail needs to
    be wrapped in a function to prevent it from being evaluated unnecessarily.

--- lib/laziness.ex
+++ lib/laziness.ex
@@ -3,38 +3,46 @@ defmodule Laziness do
     defstruct head: nil, tail: nil
   end
   
-  # Use a macro for lazy evaluation
-  defmacro cons(h, t) do
+  defmacrop ld(x) do
     quote do
-      %Cons{ head: fn () -> unquote(h) end, tail: unquote(t) }
+      fn -> unquote(x) end
     end
   end
   
+  # Use a macro for lazy evaluation
+  defmacro cons(h, t) do
+    quote do
+      %Cons{ head: fn () -> unquote(h) end, tail: fn () -> unquote(t) end }
+    end
+  end
+  
+  defp terminal, do: fn -> [] end
+  
   # Exercise 1
   def to_list([]), do: []
-  def to_list(%Cons{ head: head, tail: tail}), do: [ head.() | to_list(tail) ]
+  def to_list(%Cons{ head: h, tail: t}), do: [ h.() | to_list(t.()) ]
 
   # Exercise 2
   # Build up a new cons consisting of the elements to take
   def take([], _n), do: []
   def take(%Cons{head: h, tail: t}, n) do
-    if (n > 0), do: %Cons{head: h, tail: take(t, n - 1)}, else: []
+    if (n > 0), do: %Cons{head: h, tail: ld( take(t.(), n - 1) )}, else: []
   end
   
   def drop([], n), do: []
   def drop(stream, 0), do: stream
-  def drop(%Cons{head: _h, tail: t}, n), do: drop(t, n - 1)
+  def drop(%Cons{head: _h, tail: t}, n), do: drop(t.(), n - 1)
 
   # Exercise 3
   def take_while([], _f), do: []
   def take_while(%Cons{head: h, tail: t}, f) do
-    if (f.(h.())), do: %Cons{head: h, tail: take_while(t, f)}, else: []
+    if (f.(h.())), do: %Cons{head: h, tail: take_while(t.(), f)}, else: terminal
   end
   
   # Adapted from the text
   def fold_right([], acc, _f), do: acc
   def fold_right(%Cons{head: h, tail: t}, acc, f) do
-    f.(h.(), fn -> fold_right(t, acc, f) end)
+    f.(h.(), fn -> fold_right(t.(), acc, f) end)
   end
   
   # Exercise 4
@@ -55,4 +63,11 @@ defmodule Laziness do
   def head_option(l), do: fold_right(l, {:error, "Empty list"}, fn
     (x, acc) -> {:ok, x} end
   )
+  
+  # Exercise 7 - map
+  # I realize I've made a mistake, tail needs to be lazy
+  def map(s, f), do: fold_right(s, [], fn
+    (x, acc) -> %Cons{head: ld(f.(x)), tail: acc} end
+  )
+
 end
--- test/laziness_test.exs
+++ test/laziness_test.exs
@@ -22,17 +22,17 @@ defmodule LazinessTest do
   end
   
   test "take" do
-    assert L.take(build_stream_with_sentinel, 2) |> L.to_list == [1, 2]
+    assert L.take(build_stream_with_sentinel, 2) |>  L.to_list == [1, 2]
   end
   
   test "drop" do
     assert L.drop(build_stream, 2)|> L.to_list == [3]
   end
-  
+
   test "take_while" do
     assert L.take_while(build_stream_with_sentinel, &(&1 < 2)) |> L.to_list == [1]
   end
-  
+
   test "for_all with matching stream" do
     assert L.for_all(build_stream, &(&1 < 4)) == true
   end
@@ -40,11 +40,11 @@ defmodule LazinessTest do
   test "for_all with non-matching stream" do
     assert L.for_all(build_stream_with_sentinel, &(&1 < 2)) == false
   end
-  
+
   test "take_while_via_fold" do
     assert L.take_while_via_fold(build_stream_with_sentinel, &(&1 < 2)) == [1]
   end
-  
+
   test "head_option" do
     assert L.head_option(build_stream_with_sentinel) == {:ok, 1}
   end
@@ -52,4 +52,13 @@ defmodule LazinessTest do
   test "head_option with empty list" do
     assert L.head_option([]) == {:error, "Empty list"}
   end
+  
+  test "map" do
+    assert L.map(build_stream, &(&1 * &1))|> L.to_list == [1, 4, 9]
+  end
+
+  test "map is lazy" do
+    assert L.map(build_stream_with_sentinel, &(&1 * &1)) |> L.take(2) |> L.to_list == [1, 4]
+  end
+
 end
